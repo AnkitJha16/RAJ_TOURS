2df6503ba1174fa088eb72b194a4c1cc28219088
Ankit <freelancer.ankit.jha@gmail.com>
2023-10-18 20:24:03 +0530

    set trust proxy to true

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const compression = require('compression');
-const cors = require('cors');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const compression = require("compression");
+const cors = require("cors");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 // const xss = require('xss')
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes');
-const bookingRouter = require('./routes/bookingRoutes');
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
+const bookingRouter = require("./routes/bookingRoutes");
 
 const app = express();
 
-// app.enable('trust proxy');
+app.enable("trust proxy");
 // 1) GLOBAL MIDDLEWARES
 
 app.use(cors());
@@ -29,30 +29,30 @@ app.use(cors());
 //     origin: 'https://www.natours.com', // ( front end part)
 //   }),
 // );
-app.options('*', cors());
+app.options("*", cors());
 // app.options('/api/v1/tours/:id', cors());   for custom only access
 
 // Set Security HTTP Headers
 app.use(helmet());
 
 // Development Logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Limit requests from same API
 const limiter = rateLimit({
   max: 300,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP , please try again in an hour!',
+  message: "Too many requests from this IP , please try again in an hour!",
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // Body parser , reading data from body into req.body
 app.use(
   express.json({
-    limit: '10kb',
-  }),
+    limit: "10kb",
+  })
 );
 
 // data Sanitization against NOSQL query injection
@@ -64,14 +64,14 @@ app.use(mongoSanitize());
 app.use(
   hpp({
     whitelist: [
-      'duration',
-      'ratingsQuantity',
-      'ratingsAverage',
-      'maxGroupSize',
-      'difficulty',
-      'price',
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
     ],
-  }),
+  })
 );
 
 // compressing responses
@@ -88,14 +88,14 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/reviews', reviewRouter);
-app.use('/api/v1/bookings', bookingRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/bookings", bookingRouter);
 // this is placed at last so that all the routes are passed and not found above.
 // if it will placed above then everytime fail will be response even if the route is correct and defined.
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   // Type 1
   // res.status(404).json({
   //   status: 'fail',
