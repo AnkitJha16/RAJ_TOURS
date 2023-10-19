a191fc0303e52612d8d6fb0e0c4a2d4bfcd33a0e
Ankit <freelancer.ankit.jha@gmail.com>
2023-10-19 14:22:14 +0530

    minro changes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const reviewController = require('../controllers/reviewController');
-const authController = require('../controllers/authController');
+const express = require("express");
+const reviewController = require("../controllers/reviewController");
+const authController = require("../controllers/authController");
 
 // mergeParams = true helps to include the params from other routes also in the current router
 const router = express.Router({ mergeParams: true });
@@ -12,23 +12,25 @@ const router = express.Router({ mergeParams: true });
 
 router.use(authController.protect);
 
-router.route('/').get(reviewController.getAllReviews).post(
-  // authController.protect,
-  authController.restrictTo('user'),
-  reviewController.setTourUserIds,
-  reviewController.createReview,
-);
+router
+  .route("/")
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.restrictTo("user"),
+    reviewController.setTourUserIds,
+    reviewController.createReview
+  );
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(reviewController.getReview)
   .patch(
-    authController.restrictTo('user', 'admin'),
-    reviewController.updateReview,
+    authController.restrictTo("user", "admin"),
+    reviewController.updateReview
   )
   .delete(
-    authController.restrictTo('user', 'admin'),
-    reviewController.deleteReview,
+    authController.restrictTo("user", "admin"),
+    reviewController.deleteReview
   );
 
 module.exports = router;
